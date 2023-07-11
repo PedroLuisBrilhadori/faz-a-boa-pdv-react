0e1376614ce0e0467c10a3129a53fcd7f16a6620
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-07-10 21:45:30 -0300

    chore: add vercel config

--- src/pages/compra/index.tsx
+++ src/pages/compra/index.tsx
@@ -1,7 +1,11 @@
 import { Page } from "@/components";
 
 const Compras = () => {
-  return <Page align="center">teste</Page>;
+  return (
+    <Page align="center">
+      <h1>Nova Compra</h1>
+    </Page>
+  );
 };
 
 export default Compras;
