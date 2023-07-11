f5dcff346a3e029321b850ddb190843244f2807d
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-07-11 00:19:39 -0300

    fix: history not exported

---
+++ src/pages/historico/index.tsx
@@ -0,0 +1,11 @@
+import { Page } from "@/components";
+
+const Historico = () => {
+  return (
+    <Page align="center">
+      <div> teste </div>
+    </Page>
+  );
+};
+
+export default Historico;
